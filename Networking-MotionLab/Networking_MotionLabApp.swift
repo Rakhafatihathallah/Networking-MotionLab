081a3e8d190223a2f7bd0cfd5fb9e78e8bb30680
Rakha Fatih <rakhafatihathallah@gmail.com>
2024-02-14 19:46:20 +0700

    Initial Commit

---
+++ Networking-MotionLab/Networking_MotionLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_MotionLabApp.swift
+//  Networking-MotionLab
+//
+//  Created by Rakha Fatih Athallah on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Networking_MotionLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
